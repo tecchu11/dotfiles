01474aa4696d99a3379ec38316096934f1a4e64c
tecchu11 <agdjmp.ajtt@gmail.com>
2022-04-30 02:00:55 +0900

    Fix deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
+BLUE="\033[0;34m"
+RED="\033[0;31m"
+NC="\033[0m"
+
 # Validation
 if [ "$(uname)" != "Darwin" ] ; then
-  echo "Only install possible on os x"
+  echo -e "$RED Only install possible on os x $NC \n"
   exit 1
 fi
 
 # Install Zinit (See https://github.com/zdharma-continuum/zinit)
-echo "Install Zinit"
+echo -e "$BLUE Install Zinit $NC \n"
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 zinit self-update
 
 # Install HomeBrew (See https://brew.sh/)
-echo "Install HomeBrew"
+echo -e "$BLUE Install HomeBrew $NC \n"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # link dotfiles
 WORK_DIR="$(cd "$(dirname "$0")" && pwd )"
-echo "Link dotfiles"
+echo -e "$BLUE Link dotfiles $NC \n"
 for dotfile in "$WORK_DIR"/.??* ; do
   [ "$dotfile" = "$WORK_DIR"/.idea ] && continue
-  [ "$dotfile" == "$WORK_DIR"/.git ] && continue
-  [ "$dotfile" == "$WORK_DIR"/.github ] && continue
+  [ "$dotfile" = "$WORK_DIR"/.git ] && continue
+  [ "$dotfile" = "$WORK_DIR"/.github ] && continue
 
-  ln -fns "$dotfile" "$HOME"
+  ln -fnsv "$dotfile" "$HOME"
 done
 
-echo "Installation done"
+echo -e "$BLUE Installation done \n Run source .zshrc $NC"
