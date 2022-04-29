598e3448a6048e6278c8faac4bbbe17c98e5d0a1
tecchu11 <agdjmp.ajtt@gmail.com>
2022-04-30 01:55:06 +0900

    Fix deployment scripts

--- deploy.sh
+++ deploy.sh
@@ -1,34 +1,29 @@
 #!/bin/bash
 
 # Validation
-if [[ "$(uname)" != "Darwin" ]] ; then
+if [ "$(uname)" != "Darwin" ] ; then
   echo "Only install possible on os x"
   exit 1
 fi
 
-# Install Zinit
-#bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-if [[ -f "$(zinit)" ]]; then
-  echo "Install Zinit"
-else
-  echo "Zinit already installed"
-fi
+# Install Zinit (See https://github.com/zdharma-continuum/zinit)
+echo "Install Zinit"
+bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
+zinit self-update
 
-# Install HomeBrew ( See https://brew.sh/)
-if [[ -f "$(brew --prefix)" ]]; then
-  echo "Install HomeBrew"
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-else
-  echo "HomeBrew already installed"
-fi
+# Install HomeBrew (See https://brew.sh/)
+echo "Install HomeBrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # link dotfiles
 WORK_DIR="$(cd "$(dirname "$0")" && pwd )"
-
+echo "Link dotfiles"
 for dotfile in "$WORK_DIR"/.??* ; do
-  [[ $dotfile == $WORK_DIR/.idea ]] && continue
-  [[ $dotfile == $WORK_DIR/.git ]] && continue
-  [[ $dotfile == $WORK_DIR/.github ]] && continue
+  [ "$dotfile" = "$WORK_DIR"/.idea ] && continue
+  [ "$dotfile" == "$WORK_DIR"/.git ] && continue
+  [ "$dotfile" == "$WORK_DIR"/.github ] && continue
 
-  ln -fnsv "$dotfile" "$HOME"
+  ln -fns "$dotfile" "$HOME"
 done
+
+echo "Installation done"
