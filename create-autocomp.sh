9e6a110f933c158bb6a4854495978809c55b5853
tecchu11 <agdjmp.ajtt@gmail.com>
2023-06-05 22:35:07 +0900

    update

--- create-autocomp.sh
+++ create-autocomp.sh
@@ -4,3 +4,4 @@ kubectl completion zsh > $(brew --prefix)/share/zsh/site-functions/_kubectl
 npm completion zsh > $(brew --prefix)/share/zsh/site-functions/_npm
 kind completion zsh > $(brew --prefix)/share/zsh/site-functions/_kind
 kustomize completion zsh > $(brew --prefix)/share/zsh/site-functions/_kustomize
+helm completion zsh > $(brew --prefix)/share/zsh/site-functions/_helm
