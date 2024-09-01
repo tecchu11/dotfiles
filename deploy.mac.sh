c5275308bb8f0d2eada4475e3b66192fae1e6c62
tecchu11 <agdjmp.ajtt@gmail.com>
2024-09-01 08:34:54 +0000

    wip

--- deploy.mac.sh
+++ deploy.mac.sh
@@ -4,12 +4,6 @@ BLUE="\033[0;34m"
 RED="\033[0;31m"
 NC="\033[0m"
 
-# Validation
-if [ "$(uname)" != "Darwin" ] ; then
-  echo "$RED [ERROR] Only install possible on os x $NC"
-  exit 1
-fi
-
 # Install Zinit (See https://github.com/zdharma-continuum/zinit)
 echo "$BLUE [INFO] Install Zinit $NC"
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
@@ -20,8 +14,8 @@ echo "$BLUE [INFO] Install HomeBrew $NC"
  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # link dotfiles
-WORK_DIR="$(cd "$(dirname "$0")" && pwd )"
-echo "$BLUE [INFO] Link dotfiles $NC"
+WORK_DIR="$(cd "$(dirname "$0")" && pwd )/mac"
+echo "$BLUE [INFO] Link dotfiles on $WORK_DIR $NC"
 for dotfile in "$WORK_DIR"/.??* ; do
   [ "$dotfile" = "$WORK_DIR"/.idea ] && continue
   [ "$dotfile" = "$WORK_DIR"/.git ] && continue
@@ -31,4 +25,6 @@ for dotfile in "$WORK_DIR"/.??* ; do
   ln -fnsv "$dotfile" "$HOME"
 done
 
-echo "$BLUE [INFO] Installation done. Run source .zshrc $NC"
+/bin/zsh -c "$(source $HOME/.zshrc)"
+
+echo "$BLUE [INFO] Installation done.$NC"
