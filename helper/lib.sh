6c96e521be8fa531e936df38de86a09f8a61fa55
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2025-02-23 01:24:55 +0900

    fix (#13)

--- helper/lib.sh
+++ helper/lib.sh
@@ -16,7 +16,7 @@ install_brew() {
 
 install_asdf() {
 	echo "[INFO] Install asdf"
-	curl -o asdf.tar.gz -L https://github.com/asdf-vm/asdf/releases/download/v0.16.4/asdf-v0.16.4-darwin-arm64.tar.gz
+	curl -o asdf.tar.gz -L https://github.com/asdf-vm/asdf/releases/download/v0.16.4/asdf-v0.16.4-linux-arm64.tar.gz
 	sudo tar -xzf asdf.tar.gz -C /usr/local/bin
 	rm asdf.tar.gz
 }
